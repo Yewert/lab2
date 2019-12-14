a0142e8b14f545a596ac814b686096764e7a8bc8
Yewert <blazer.andrew@gmail.com>
2019-12-14 13:04:26 +0500

    added function stubs

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -1,14 +1,21 @@
 #include "button.h"
+#include <Servo.h>
+#include "LedControl.h"
 
 #define BUTTON_PIN 10
+#define SERVO_PIN
 
+
+Servo servo;
 Button button(BUTTON_PIN);
 bool wasButtonDown = false;
 int buttonDownFor = -1;
+const int servoInitialPosition = 0;
 
 void setup()
 {
-  
+  servo.attach(SERVO_PIN);
+  servo.write(servoInitialPosition);
 }
 
 void loop()
@@ -25,7 +32,28 @@ void loop()
     return;
   }
   
-  //hit
+  hit(buttonDownFor % 100);
   //reset servo
   //reset display
+}
+
+void hit(int power)
+{
+  //todo
+  servo.write(90);
+}
+
+void resetServo()
+{
+  servo.write(servoInitialPosition);
+}
+
+void print(int power)
+{
+  //todo
+}
+
+void resetDisplay()
+{
+  //todo
 }
